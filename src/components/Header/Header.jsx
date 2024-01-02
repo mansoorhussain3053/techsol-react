0a2b447bf9ecbe2ba19a6bec2c04a188645e9e34
Mansoor Hussain <mansoorhussain3053@gmail.com>
2024-01-03 02:03:49 +0500

    Created Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,25 @@
 import './header.css'
+import Logo from '../../assets/techlogo.png'
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className='Container__Header'>
+      <div className='Header__logo'>
+        <img src={Logo} />
+      </div>
+      <div className="Header__nav">
+      <ul className='Header__nav-lists'>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Services</a></li>
+      <li><a href="#">Portfolio</a></li>
+      <li><a href="#">Blog</a></li>
+      <li><a href="#">About us</a></li>
+      <li><a href="#">Contact us</a></li>
+      </ul>
+      </div>
+      <div className="Header__button">
+      <button>Get Started</button>
+      </div>
     </div>
   )
 }
