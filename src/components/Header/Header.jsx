8efb821b393246a9441d8681142c310c70878987
Mansoor Hussain <mansoorhussain3053@gmail.com>
2024-01-03 03:53:16 -0800

    Hero Section Ready

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,40 @@
-import './header.css'
-import Logo from '../../assets/techlogo.png'
+import "./header.css";
+import Logo from "../../assets/techlogo.png";
 
 const Header = () => {
   return (
-    <div className='Container__Header'>
-      <div className='Header__logo'>
+    <div className="Container__Header">
+      <div className="header-icon">
         <img src={Logo} />
       </div>
+      
       <div className="Header__nav">
-      <ul className='Header__nav-lists'>
-      <li><a href="#">Home</a></li>
-      <li><a href="#">Services</a></li>
-      <li><a href="#">Portfolio</a></li>
-      <li><a href="#">Blog</a></li>
-      <li><a href="#">About us</a></li>
-      <li><a href="#">Contact us</a></li>
-      </ul>
+        <ul className="Header__nav-lists">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Services</a>
+          </li>
+          <li>
+            <a href="#">Portfolio</a>
+          </li>
+          <li>
+            <a href="#">Blog</a>
+          </li>
+          <li>
+            <a href="#">About us</a>
+          </li>
+          <li>
+            <a href="#">Contact us</a>
+          </li>
+        </ul>
       </div>
       <div className="Header__button">
-      <button>Get Started</button>
+        <button>Get Started</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
