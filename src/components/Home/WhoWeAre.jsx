3c6dad8f89cfdfdced03f45f24c1a2d5ec53f589
Mansoor Hussain <mansoorhussain3053@gmail.com>
2024-01-08 01:33:34 +0500

    Who we are section is ready

--- src/components/Home/WhoWeAre.jsx
+++ src/components/Home/WhoWeAre.jsx
@@ -1,6 +1,10 @@
 import './whoweare.css'
 import IMAGE from '../../assets/Digitalconsulting.jpg'
 import ANUAL from '../../assets/annualicon.png'
+import { IoIosArrowDropright } from "react-icons/io";
+import { FaRegArrowAltCircleRight } from "react-icons/fa";
+
+
 
 const WhoWeAre = () => {
   return (
@@ -8,14 +12,14 @@ const WhoWeAre = () => {
       
       <div className='Container__Digital'>
           <h3>DIGITAL CONSULTING</h3>
-          <h2>Unique Business Consulting Agency corporate.</h2>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo. dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+          <h2>Unique Business Consulting <br/><span className='Digital__Span'> Agency</span> corporate.</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo. dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
         <ul>
-          <li>Branding the latest marketing agency.</li>
-          <li>Branding the latest marketing agency.</li>
-          <li>Branding the latest marketing agency.</li>
+          <li><IoIosArrowDropright className='Lists__Icon'/>Branding the latest marketing agency.</li>
+          <li><IoIosArrowDropright className='Lists__Icon'/>Connect to more online customers.</li>
+          <li><IoIosArrowDropright className='Lists__Icon'/>Grow up your digital business.</li>
           </ul>
-          <button>Discover More</button>
+          <button className='Digital__Button'>Discover More <FaRegArrowAltCircleRight/></button>
       </div>
 
 
@@ -25,11 +29,14 @@ const WhoWeAre = () => {
 <div className='Main__Icon'>
 
         <div className='Annual__Icon'>
-          <i><img src={ANUAL} className='Icon__IMG' /></i>
+          <img src={ANUAL} className='Icon__IMG' />
         </div>
         
         <div className='Annual__Content'>
-          <article className='Progress__Bar'></article>
+          <h3>ANNUAL</h3>
+          <p>87%</p>
+          <article className='Progress__Bar'><progress value={0.9} label={45} />
+</article>
           <h4>Business Growth</h4>
         </div>
 
