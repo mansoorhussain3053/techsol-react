1943d28878edacdefb3ee0ff39974c52621ed916
Mansoor Hussain <mansoorhussain3053@gmail.com>
2024-01-08 00:41:00 -0800

    2 components are ready to launch

--- src/components/Home/HeroSection.jsx
+++ src/components/Home/HeroSection.jsx
@@ -1,5 +1,4 @@
 import './herosection.css'
-import React from 'react';
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
 
@@ -11,8 +10,7 @@ const contentSlides = [
     },
     {
       title: 'Business Value through digital products',
-      description: 'Trusted by the world’s most established brands',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est cum laboriosam odio consequuntur, doloribus dolorum repellendus facere sed sequi fugiat ullam nihil harum reprehenderit accusantium. Reprehenderit omnis accusamus blanditiis eum.',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero esse fugit velit accusamus dignissimos in perferendis obcaecati. Minima, rerum eius.'
     },
     {
       title: 'Digital Design Awards',
@@ -25,12 +23,47 @@ const contentSlides = [
     // Add more slides as needed
   ];
   
+  const buttonStyle = {
+    width: "30px",
+    background: 'none',
+    border: '0px'
+  };
+  
+  const properties = {
+    prevArrow: (
+      <button style={{ ...buttonStyle }}>
+
+
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="Icon__Left">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+</svg>
+
+
+
+
+
+
+      </button>
+    ),
+    nextArrow: (
+      <button style={{ ...buttonStyle }}>
+        
+
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="Icon__Right">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+</svg>
+
+
+      </button>
+    )
+  };
+  
   
   const HeroSection = () => {
     return (
       <div className="slide-container">
         
-        <Slide>
+        <Slide {...properties}>
           {contentSlides.map((slide, index) => (
             <div key={index} className="each-slide">
               <div className="slide-content-container">
