ebe97dc6f34e88ad2d4274bafba9b093f5d06127
Mansoor Hussain <mansoorhussain3053@gmail.com>
2024-01-09 00:43:06 -0800

    Removed the trash

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,74 @@
-import './App.css'
-import Footer from './components/Footer/Footer'
-import Header from './components/Header/Header'
-import ExpressEver from './components/Home/ExpressEver'
-import GrowBusiness from './components/Home/GrowBusiness'
-import HeroSection from './components/Home/HeroSection'
-import HowItWorks from './components/Home/HowItWorks'
-import NeedMoreHelp from './components/Home/NeedMoreHelp'
-import Progress from './components/Home/Progress'
-import Testimonials from './components/Home/Testimonials'
-import WhatWEOffer from './components/Home/WhatWEOffer'
-import WhoWeAre from './components/Home/WhoWeAre'
-import WhyChooseUs from './components/Home/WhyChooseUs'
-import AnimatedCursor from "react-animated-cursor"
-
-
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import ExpressEver from "./components/Home/ExpressEver";
+import GrowBusiness from "./components/Home/GrowBusiness";
+import HeroSection from "./components/Home/HeroSection";
+import HowItWorks from "./components/Home/HowItWorks";
+import NeedMoreHelp from "./components/Home/NeedMoreHelp";
+import Progress from "./components/Home/Progress";
+import Testimonials from "./components/Home/Testimonials";
+import WhatWEOffer from "./components/Home/WhatWEOffer";
+import WhoWeAre from "./components/Home/WhoWeAre";
+import WhyChooseUs from "./components/Home/WhyChooseUs";
+import AnimatedCursor from "react-animated-cursor";
 
 function App() {
   return (
     <>
+      <Header />
+      <HeroSection />
+      <AnimatedCursor
+        className="Cursor__Hidden"
+        innerSize={24}
+        outerSize={20}
+        z-index={99}
+        color="69,242,252"
+        outerAlpha={0.2}
+        innerScale={0.7}
+        innerStyle={{
+          background:
+            "radial-gradient(circle, rgba(63,94,251,1) 0%, rgba(252,70,107,1) 100%)",
+        }}
+        outerScale={5}
+        clickables={[
+          "a",
+          'input[type="text"]',
+          'input[type="email"]',
+          'input[type="number"]',
+          'input[type="submit"]',
+          'input[type="image"]',
+          "label[for]",
+          "select",
+          "textarea",
+          "button",
+          ".link",
+          {
+            target: ".custom",
+            options: {
+              innerSize: 15,
+              outerSize: 15,
+              color: "131,58,180",
+              outerAlpha: 0.3,
+              innerScale: 0.7,
+              outerScale: 5,
+            },
+          },
+        ]}
+      />
 
-    <Header/>
-    <HeroSection/>
-    <AnimatedCursor className="Cursor__Hidden"
-      innerSize={24}
-      outerSize={20}
-      z-index={99}
-
-      color='69,242,252'
-      outerAlpha={0.2}
-      innerScale={0.7}
-      innerStyle={{
-        background: 'radial-gradient(circle, rgba(63,94,251,1) 0%, rgba(252,70,107,1) 100%)'
-      }}
-      outerScale={5}
-      clickables={[
-        'a',
-        'input[type="text"]',
-        'input[type="email"]',
-        'input[type="number"]',
-        'input[type="submit"]',
-        'input[type="image"]',
-        'label[for]',
-        'select',
-        'textarea',
-        'button',
-        '.link',
-        {
-          target: '.custom',
-          options: {
-            innerSize: 15,
-            outerSize: 15,
-            color: '131,58,180',
-            outerAlpha: 0.3,
-            innerScale: 0.7,
-            outerScale: 5
-          }
-        }
-      ]}
-    />
-    
-    <WhoWeAre/>
-    <Progress/>
-    <WhatWEOffer/>
-    <Testimonials/>
-    <ExpressEver/>
-    <HowItWorks/>
-    <WhyChooseUs/>
-    <NeedMoreHelp/>
-    <GrowBusiness/>
-    <Footer/>
-
+      <WhoWeAre />
+      <Progress />
+      <WhatWEOffer />
+      <Testimonials />
+      <ExpressEver />
+      <HowItWorks />
+      <WhyChooseUs />
+      <NeedMoreHelp />
+      <GrowBusiness />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Home/HeroSection.jsx
+++ src/components/Home/HeroSection.jsx
@@ -1,84 +1,20 @@
 import './herosection.css'
-import { Slide } from 'react-slideshow-image';
-import 'react-slideshow-image/dist/styles.css';
 
-
-const contentSlides = [
-    {
-      title: 'Original Solutions',
-      description: 'If you need to redesign your new project, new visual strategy, ux structure or you do have some cool ideas for collaboration.',
-    },
-    {
-      title: 'Business Value through digital products',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero esse fugit velit accusamus dignissimos in perferendis obcaecati. Minima, rerum eius.'
-    },
-    {
-      title: 'Digital Design Awards',
-      description: 'If you need to redesign your new project, new visual strategy, ux structure or you do have some cool ideas for collaboration.',
-    },
-    {
-      title: 'Trusted by the world’s most established brands',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est cum laboriosam odio consequuntur, doloribus dolorum repellendus facere sed sequi fugiat ullam nihil harum reprehenderit accusantium. Reprehenderit omnis accusamus blanditiis eum.',  
-    },
-    // Add more slides as needed
-  ];
-  
-  const buttonStyle = {
-    width: "30px",
-    background: 'none',
-    border: '0px'
-  };
-  
-  const properties = {
-    prevArrow: (
-      <button style={{ ...buttonStyle }}>
-
-
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="Icon__Left">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
-</svg>
-
-
-
-
-
-
-      </button>
-    ),
-    nextArrow: (
-      <button style={{ ...buttonStyle }}>
-        
-
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="Icon__Right">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
-</svg>
-
-
-      </button>
-    )
-  };
-  
-  
   const HeroSection = () => {
     return (
       <div className="slide-container">
         
-        <Slide {...properties}>
-          {contentSlides.map((slide, index) => (
-            <div key={index} className="each-slide">
+            <div className="each-slide">
               <div className="slide-content-container">
-                <h2>{slide.title}</h2>
-                <p>{slide.description}</p>
+                <h2>Digital Design Awards</h2>
+                <p>If you need to redesign your new project, new visual strategy, ux structure or you do have some cool ideas for collaboration.</p>
                 <button type='button' className='Hover__Button'><span className='Button__Span'> </span>Learn More</button>
                 
               </div>
               
             </div>
-          ))}
-        </Slide>
       </div>
     );
-    
     
   };
   
