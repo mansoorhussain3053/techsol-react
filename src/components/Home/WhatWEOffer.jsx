ca7b7a80f115a4ce163c3de39880eb0531aad0da
Mansoor Hussain <mansoorhussain3053@gmail.com>
2024-01-09 05:58:55 -0800

    What we offer Ready!

--- src/components/Home/WhatWEOffer.jsx
+++ src/components/Home/WhatWEOffer.jsx
@@ -1,9 +1,56 @@
 import './whatweoffer.css'
+import { MdOutlineDesignServices } from "react-icons/md";
+import { HiLightBulb } from "react-icons/hi";
+import { FaRegArrowAltCircleRight } from "react-icons/fa";
+import { FaLaptopCode } from "react-icons/fa";
+
+
+
 
 const WhatWEOffer = () => {
-  return (
-    <div>
-      What We offer
+  return (  
+        
+     
+    
+    <div className='Services__Parent'>
+      <div className='Line_Top_Parent'><div className='Line__Top'></div></div>
+
+      <div className='Upper__Line'>
+          <span className='Line__Services'></span>
+      </div>
+
+
+        <div className='Services__Heading'>
+          <h3>BEST FEATURES</h3>
+          <h2>We are a new digital product development <span className='Service__Span'> Agency</span></h2>
+        </div>
+
+        <div className='Services__Divs'>
+
+          <article> <MdOutlineDesignServices className='Services__Icons'/>
+           <h3>Graphic <br />Clean Design</h3>
+           <p>Tempore corrupti temporibus fuga earum asperiores fugit.</p>
+          </article>
+
+          <article> <FaLaptopCode className='Services__Icons'/>
+           <h3>Web & <br />Mobile Design</h3>
+           <p>Tempore corrupti temporibus fuga earum asperiores fugit.</p>
+          </article>
+
+          <article> <HiLightBulb className='Services__Icons'/>
+           <h3>Social <br />Media Marketing</h3>
+           <p>Tempore corrupti temporibus fuga earum asperiores fugit.</p>
+          </article>
+          
+          
+
+
+        </div>
+
+        <button className='Services__Button'>Discover More <FaRegArrowAltCircleRight/></button>
+
+
+        <div className='Line_Down_Parent'><div className='Line__Down'></div></div>
     </div>
   )
 }
