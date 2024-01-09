d4529d7f7511c1a3fda95a58e1d2e0286b0fb0d1
Mansoor Hussain <mansoorhussain3053@gmail.com>
2024-01-09 08:00:56 -0800

    Testimonials Next

--- src/components/Home/Testimonials.jsx
+++ src/components/Home/Testimonials.jsx
@@ -1,10 +1,28 @@
 
+import 'react-circular-progressbar/dist/styles.css';
 import './testimonials.css'
 
 const Testimonials = () => {
+  
   return (
-    <div>
-      Testimonials
+    <div className='Projects__Parent'>
+      <div className='Projects__Counter'>
+
+ 
+      <div>
+       TEstimonials Remaining
+          
+        </div>
+      </div>
+
+
+
+
+
+      <div className='Testimonials__Parent'>
+
+      </div>
+
     </div>
   )
 }
