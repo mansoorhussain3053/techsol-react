b86a5459ae9ec1c3b002fdf9def3de40cbb30a2f
Mansoor Hussain <mansoorhussain3053@gmail.com>
2024-01-10 05:46:05 -0800

    Testimonials

--- src/components/Home/Testimonials.jsx
+++ src/components/Home/Testimonials.jsx
@@ -1,29 +1,134 @@
-
-import './testimonials.css'
+import "./testimonials.css";
 
 const Testimonials = () => {
-  
   return (
-    <div className='Projects__Parent'>
-      <div className='Projects__Counter'>
+    <div className="Projects__Parent">
+      <div className="Projects__Counter">
 
- 
-      <div>
-       TEstimonials Remaining
-          
+
+            
+
+
+            <div className="Circle__Parent">
+
+                <div className="Circle__Content">
+                <p>Project</p>
+                <h3>Consulting</h3>
+              
+                </div>
+
+               <div className="circle-border">
+                  <div className="circle"><p>90%</p></div>
+              </div>
+            </div>
+
+
+            <div className="Circle__Parent">
+
+              <div className="Circle__Content">
+              <p>Project</p>
+              <h3>Consulting</h3>
+
+              </div>
+
+              <div className="circle-border">
+                <div className="circle"><p>78%</p></div>
+              </div>
+              </div>
+
+
+
+              <div className="Circle__Parent">
+
+                <div className="Circle__Content">
+                <p>Project</p>
+                <h3>Consulting</h3>
+              
+                </div>
+
+               <div className="circle-border">
+                  <div className="circle"><p>94%</p></div>
+              </div>
+            </div>
+
+
+
+
+            <div className="Circle__Parent">
+
+                <div className="Circle__Content">
+                <p>Project</p>
+                <h3>Consulting</h3>
+              
+                </div>
+
+               <div className="circle-border">
+                  <div className="circle"><p>95%</p></div>
+              </div>
+            </div>
+
+
+
+
+            <div className="Circle__Parent">
+
+                <div className="Circle__Content">
+                <p>Project</p>
+                <h3>Consulting</h3>
+              
+                </div>
+
+               <div className="circle-border">
+                  <div className="circle"><p>88%</p></div>
+              </div>
+            </div>
+
+
+
+
+            <div className="Circle__Parent">
+
+                <div className="Circle__Content">
+                <p>Project</p>
+                <h3>Consulting</h3>
+              
+                </div>
+
+               <div className="circle-border">
+                  <div className="circle"><p>86%</p></div>
+              </div>
+            </div>
+
+             
+             
+                
+
+                
+
+      </div>
+
+
+      <div className="Testimonials__Parent">
+        <div className="Testimonials__Slides">
+          <div>
+        <h3>OUR CLIENTS</h3>
+        <h2>What Clients Say?</h2>
         </div>
-      </div>
 
+                <div>
+                  <p>Nulla metus metus ullamcorper vel tincidunt sed euismod nibh volutpat velit class aptent taciti sociosqu ad litora.</p>
+                </div>
 
+                <div>
+                  <img src="" alt="" />
+                  <h3>Alex Regelman</h3>
+                  <p>Co-Founder, Colabrio</p>
+                </div>
 
-
-
-      <div className='Testimonials__Parent'>
-
-      </div>
-
+        </div>
+        </div>
     </div>
-  )
-}
+  );
+};
 
-export default Testimonials
+export default Testimonials;
