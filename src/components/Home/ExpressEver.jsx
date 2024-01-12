7c003eab42b037525553079df68d4cfc2fd9f229
Mansoor Hussain <mansoorhussain3053@gmail.com>
2024-01-13 03:31:42 +0500

    Added a slider in testimonials

--- src/components/Home/ExpressEver.jsx
+++ src/components/Home/ExpressEver.jsx
@@ -1,9 +1,42 @@
 import './expressever.css'
+import CLIENT1 from '../../assets/clientlogos/client1.webp'
+import CLIENT2 from '../../assets/clientlogos/client2.webp'
+import CLIENT3 from '../../assets/clientlogos/client3.webp'
+import CLIENT4 from '../../assets/clientlogos/client4.webp'
+import CLIENT5 from '../../assets/clientlogos/client5.webp'
+import CLIENT6 from '../../assets/clientlogos/client6.webp'
+import CLIENT7 from '../../assets/clientlogos/client7.webp'
+import CLIENT8 from '../../assets/clientlogos/client8.webp'
 
 const ExpressEver = () => {
   return (
-    <div>
-      Express Ever
+    <div className='Clients__Parents'>
+
+
+        <div className='Clients__Heading'>
+          <h3>BEST EXPERIENCE</h3>
+          <h2>Our Clients</h2>
+          <p>Our area of practice is quite wide: design, graphics, branding, development.</p>
+        </div>
+
+        <div className='Clients__Logos'>
+          <article><a href='#'><img src={CLIENT1} className='Logo_Client__1' /></a></article>
+          <article><a href='#'><img src={CLIENT2} className='Logo_Client__2' /></a></article>
+          <article><a href='#'><img src={CLIENT3} className='Logo_Client__3' /></a></article>
+          <article><a href='#'><img src={CLIENT4} className='Logo_Client__4' /></a></article>
+          <article><a href='#'><img src={CLIENT5} className='Logo_Client__5' /></a></article>
+          <article><a href='#'><img src={CLIENT6} className='Logo_Client__6' /></a></article>
+          <article><a href='#'><img src={CLIENT7} className='Logo_Client__7' /></a></article>
+          <article><a href='#'><img src={CLIENT8} className='Logo_Client__8' /></a></article>
+
+
+          
+
+
+        </div>
+
+
+
     </div>
   )
 }
--- src/components/Home/Testimonials.jsx
+++ src/components/Home/Testimonials.jsx
@@ -1,4 +1,38 @@
 import "./testimonials.css";
+// import Swiper core and required modules
+import { Pagination } from 'swiper/modules';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+const data = [
+  {
+    name: 'Tina Snow',
+    review: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, ipsa ipsam saepe culpa voluptatem architecto non perferendis hic animi laboriosam.',
+    designation: 'Co-Founder, Colabrio'
+  },
+  {
+    name: 'Dana Lorem',
+    review: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, ipsa ipsam saepe culpa voluptatem architecto non perferendis hic animi laboriosam.',
+    designation: 'Co-Founder, Colabrio'
+  },
+  {
+    name: 'Sesha Margot',
+    review: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, ipsa ipsam saepe culpa voluptatem architecto non perferendis hic animi laboriosam.',
+    designation: 'Co-Founder, Colabrio'
+  },
+  {
+    name: 'Lisa Jack',
+    review: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, ipsa ipsam saepe culpa voluptatem architecto non perferendis hic animi laboriosam.',
+    designation: 'Co-Founder, Colabrio'
+  }
+]
+
+
 
 const Testimonials = () => {
   return (
@@ -105,27 +139,44 @@ const Testimonials = () => {
                 
 
       </div>
+      <div className="All__Slide__Parent">
 
+      <div className="Testimonial__Heading">
+                     <h3>OUR CLIENTS</h3>
+                      <h2>What Clients Say?</h2>
+                      </div>
 
-      <div className="Testimonials__Parent">
-        <div className="Testimonials__Slides">
-          <div className="Testimonial__Heading">
-        <h3>OUR CLIENTS</h3>
-        <h2>What Clients Say?</h2>
-        </div>
+      <Swiper className="Testimonials__Parent" // install Swiper modules
+      modules={[ Pagination ]}
+      spaceBetween={10}
+      slidesPerView={1}
+      pagination={{ clickable: true }}
+      >
 
-                <div>
-                  <p>Nulla metus metus ullamcorper vel tincidunt sed euismod nibh volutpat velit class aptent taciti sociosqu ad litora.</p>
-                </div>
+          {
+            data.map(({name, review, designation}, index) => {
+              return (
+              <SwiperSlide key={index} className="Testimonials__Slides">
+                 
+
+                          <div className="Testimonial__Slide__Content">
+                          <p>{review}</p>
+                              </div>
 
                 <div>
                   <img src="" alt="" />
-                  <h3>Alex Regelman</h3>
-                  <p>Co-Founder, Colabrio</p>
+                  <h3>{name}</h3>
+                  <p>{designation}</p>
                 </div>
+        </SwiperSlide>
 
-        </div>
-        </div>
+            )
+            })
+          }
+
+        
+        </Swiper>
+    </div>
     </div>
   );
 };
