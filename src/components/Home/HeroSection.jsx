7b4deef65c8df5bb16cb982e2b57b3470c4aa75c
Mansoor Hussain <mansoorhussain3053@gmail.com>
2024-01-14 16:16:01 +0500

    slides with custom arrows

--- src/components/Home/HeroSection.jsx
+++ src/components/Home/HeroSection.jsx
@@ -20,85 +20,67 @@ import { Parallax, Navigation } from 'swiper/modules';
 
   const HeroSection = () => {
     return (
+      
 
       <Swiper
         style={{
-          '--swiper-navigation-color': '#fff',
+          '--swiper-navigation-color': 'var(--color-white)',
           '--swiper-pagination-color': '#fff',
         }}
         speed={600}
         parallax={true}
-        pagination={{
-          clickable: true,
-        }}
         navigation={true}
+        
         modules={[Parallax, Navigation]}
         className="mySwiper"
       >
         <div
+        
           slot="container-start"
           className="parallax-bg"
           style={{
             'background-image':
-              'url(https://images.unsplash.com/photo-1682687982468-4584ff11f88a?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)',
+              'url("https://i.pinimg.com/originals/50/e4/3e/50e43ebcc0c1916dcd7a93cfc6bfeb6e.webp")',
           }}
           data-swiper-parallax="-23%"
-        ></div>
+        > </div>
         <SwiperSlide>
           <div className="title" data-swiper-parallax="-300">
-            Slide 1
+            <h2>Digital Design Awards</h2>
           </div>
           <div className="subtitle" data-swiper-parallax="-200">
-            Subtitle
           </div>
           <div className="text" data-swiper-parallax="-100">
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
-              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
-              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
-              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
-              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
-              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
+            If you need to redesign your new project, new visual strategy, ux structure or you do have some cool ideas for collaboration.
             </p>
+            <button>Learn More</button>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="title" data-swiper-parallax="-300">
-            Slide 2
+          <h2>Original Solutions</h2>
           </div>
           <div className="subtitle" data-swiper-parallax="-200">
-            Subtitle
           </div>
           <div className="text" data-swiper-parallax="-100">
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
-              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
-              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
-              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
-              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
-              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
+            If you need to redesign your new project, new visual strategy, ux structure or you do have some cool ideas for collaboration.
             </p>
+            <button>Learn More</button>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="title" data-swiper-parallax="-300">
-            Slide 3
+           <h2>Business Vaule <br/>through Digital Products</h2>
           </div>
           <div className="subtitle" data-swiper-parallax="-200">
-            Subtitle
           </div>
           <div className="text" data-swiper-parallax="-100">
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
-              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
-              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
-              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
-              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
-              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum at incidunt eos quasi et distinctio.
             </p>
+            <button>Learn More</button>
           </div>
         </SwiperSlide>
       </Swiper>
