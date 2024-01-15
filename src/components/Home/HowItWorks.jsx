bd5bb0d660f69a14dfb989f63d05217059f81a48
Mansoor Hussain <mansoorhussain3053@gmail.com>
2024-01-15 08:18:06 -0800

    Home portfolio in process

--- src/components/Home/HowItWorks.jsx
+++ src/components/Home/HowItWorks.jsx
@@ -1,9 +1,59 @@
 import './howitworks.css'
+import { FaLongArrowAltRight } from "react-icons/fa";
+import { FaRegArrowAltCircleRight } from "react-icons/fa";
+import portfolioimage1 from '../../assets/portfolioimages/mindpeaceenglish.webp'
+
+
 
 const HowItWorks = () => {
   return (
-    <div>
-      How it Works
+    <div className='Home__Portfolio__Parent'>
+      <div className='Home_Portfolio_heading'>
+        <h3>PORTFOLIO</h3>
+        <h2>Our Recent Web Design & <br />Some Past Projects.</h2>
+      </div>
+
+        <div className='Portfolio__Home'>
+
+
+        <article>
+          <div className='Portfolio__Home__Image' >
+            <img src={portfolioimage1} alt="" />
+          </div>
+          <h3>Development, UI UX</h3>
+          <h2>Mind Peace Psychology</h2>
+          <p>Dr. Luo, a Counseling Psychology Ph.D. graduate, offers evidence-based psychological assessment, diagnosis, and psychotherapy for adults with various issues</p>
+          <button>See Project<FaLongArrowAltRight className='Portfolio__Project-Icon' /></button>
+        </article>
+
+
+        <article>
+          <div className='Portfolio__Home__Image' >
+            <img src={portfolioimage1} alt="" />
+          </div>
+          <h3>Development, UI UX</h3>
+          <h2>Mind Peace Psychology</h2>
+          <p>Dr. Luo, a Counseling Psychology Ph.D. graduate, offers evidence-based psychological assessment, diagnosis, and psychotherapy for adults with various issues</p>
+          <button>See Project<FaLongArrowAltRight className='Portfolio__Project-Icon' /></button>
+        </article>
+
+        <article>
+          <div className='Portfolio__Home__Image' >
+            <img src={portfolioimage1} alt="" />
+          </div>
+          <h3>Development, UI UX</h3>
+          <h2>Mind Peace Psychology</h2>
+          <p>Dr. Luo, a Counseling Psychology Ph.D. graduate, offers evidence-based psychological assessment, diagnosis, and psychotherapy for adults with various issues</p>
+          <button>See Project<FaLongArrowAltRight className='Portfolio__Project-Icon' /></button>
+        </article>
+
+        
+        </div>
+        <div>
+        <button className='Home__Portfolio__Banner'>See More<FaRegArrowAltCircleRight/></button>
+        </div>
+
+
     </div>
   )
 }
--- src/components/Home/WhatWEOffer.jsx
+++ src/components/Home/WhatWEOffer.jsx
@@ -3,6 +3,7 @@ import { MdOutlineDesignServices } from "react-icons/md";
 import { HiLightBulb } from "react-icons/hi";
 import { FaRegArrowAltCircleRight } from "react-icons/fa";
 import { FaLaptopCode } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 
 
 
@@ -28,18 +29,25 @@ const WhatWEOffer = () => {
         <div className='Services__Divs'>
 
           <article> <MdOutlineDesignServices className='Services__Icons'/>
-           <h3>Graphic <br />Clean Design</h3>
-           <p>Tempore corrupti temporibus fuga earum asperiores fugit.</p>
+           <h3> Web Design & <br />Developing</h3>
+
+           <p> We start with a clear vision, creating a secure, user-friendly website tailored to your goals, ensuring ongoing support for success.
+                      </p>
           </article>
 
           <article> <FaLaptopCode className='Services__Icons'/>
-           <h3>Web & <br />Mobile Design</h3>
-           <p>Tempore corrupti temporibus fuga earum asperiores fugit.</p>
+           <h3>Mobile <br />App Dev</h3>
+           <p>
+           We excel in mobile app development, offering expertise in UI/UX design, secure coding, optimization, launch, and ongoing support for success.
+                        </p>
           </article>
 
           <article> <HiLightBulb className='Services__Icons'/>
-           <h3>Social <br />Media Marketing</h3>
-           <p>Tempore corrupti temporibus fuga earum asperiores fugit.</p>
+           <h3>Digital Branding</h3>
+           <p>We prioritize web design's visual appeal, ensuring positive experiences, seamless navigation, and responsive functionality, including rebranding services.
+
+
+                      </p>
           </article>
           
           
@@ -47,7 +55,7 @@ const WhatWEOffer = () => {
 
         </div>
 
-        <button className='Services__Button'>Discover More <FaRegArrowAltCircleRight/></button>
+       <Link to="/services"><button className='Services__Button'>Discover More <FaRegArrowAltCircleRight/></button></Link> 
 
 
         <div className='Line_Down_Parent'><div className='Line__Down'></div></div>
