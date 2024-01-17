eb732047f0ec4d2b9336d7fba317f5723856657c
Mansoor Hussain <mansoorhussain3053@gmail.com>
2024-01-17 04:57:09 -0800

    Contact Form is Functional

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -6,44 +6,116 @@ import { TbBrandFiverr } from "react-icons/tb";
 import { FaFacebook } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 
-
 const ContactForm = () => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
     message: "",
-    subject: ""
+    number: "",
+    services: "",
+    budget: "",
+    deadline: "",
+    subject: "contact us",
   });
-console.log(formData);
+  const data = new FormData();
+  data.append("name", formData.name);
+  data.append("email", formData.email);
+  data.append("message", formData.message);
+  data.append("phone", formData.number);
+  data.append("services", formData.services);
+  data.append("budget", formData.budget);
+  data.append("deadline", formData.deadline);
+  data.append("subject", formData.subject);
+
   const handleFormData = (e) => {
-    console.log(e.target);
     const { name, value } = e.target;
     setFormData((prev) => ({
       ...prev,
       [name]: value,
     }));
   };
+
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+    fetch("https://techsolllc.net/api/", {
+      // headers: {
+      //   "Content-Type": "application/json",
+      // },
+      method: "POST",
+      body: data,
+    })
+      .then((res) => {
+        if (!res.ok) {
+          console.log("ERROR");
+        }
+        return res.json();
+      })
+      .then((res) => {
+        setFormData({
+          name: "",
+          budget: "",
+          deadline: "",
+          email: "",
+          message: "",
+          subject: "",
+          number: "",
+          services: "",
+        });
+      });
+  };
+
   return (
     <div path="ContainerFrom" className="Container__Contact__Form">
       <div className="contact_Form">
         <h3>Get In Touch.</h3>
-        <form>
+        <form onSubmit={handleSubmitForm}>
+          <div className="Aligning-Fields">
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              onChange={handleFormData}
+              value={formData.name}
+            />
+            <input
+              type="text"
+              name="number"
+              placeholder="Number"
+              onChange={handleFormData}
+              value={formData.number}
+            />
+          </div>
+          <div className="Aligning-Fields">
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              onChange={handleFormData}
+              value={formData.email}
+            />
+            <select name="services" id="cars" onChange={handleFormData}>
+              <option value="Select">Select the Service</option>
+              <option value="Web Development">Web Development</option>
+              <option value="graphic design">Graphic Design</option>
+              <option value="social media management">
+                Social Media Management
+              </option>
+              <option value="app development">App Development</option>
+            </select>
+          </div>
           <input
             type="text"
-            name="name"
-            placeholder="Name" 
+            name="budget"
+            placeholder="150$ - 1000$"
             onChange={handleFormData}
+            value={formData.budget}
           />
-          <input type="text"
-           name="subject"
-            placeholder="Subject"
-            onChange={handleFormData}
-             />
           <input
-            type="email"
-            name="email"
-            placeholder="Email"
+            type="text"
+            name="deadline"
+            placeholder="Enter Your Deadline"
             onChange={handleFormData}
+            value={formData.deadline}
           />
           <textarea
             name="message"
@@ -52,6 +124,7 @@ console.log(formData);
             rows="10"
             placeholder="Message"
             onChange={handleFormData}
+            value={formData.message}
           ></textarea>
           <button type="submit">Submit</button>
         </form>
@@ -66,12 +139,31 @@ console.log(formData);
           <p>2092 Idlewood Drive, Grapevine, TX 76051​</p>
 
           <ul className="Contact__Social">
-            <li><a href=""><FaFacebook className="Contact__Social__Icons"/></a></li>
-            <li><a href=""><SiUpwork className="Contact__Social__Icons"/></a></li>
-            <li><a href=""><IoLogoGithub className="Contact__Social__Icons"/></a></li>
-            <li><a href=""><TbBrandFiverr className="Contact__Social__Icons"/></a></li>
-            <li><a href=""><FaXTwitter className="Contact__Social__Icons"/></a></li>
-            
+            <li>
+              <a href="">
+                <FaFacebook className="Contact__Social__Icons" />
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <SiUpwork className="Contact__Social__Icons" />
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <IoLogoGithub className="Contact__Social__Icons" />
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <TbBrandFiverr className="Contact__Social__Icons" />
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <FaXTwitter className="Contact__Social__Icons" />
+              </a>
+            </li>
           </ul>
         </div>
       </div>
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,10 +1,17 @@
+
 import './portfolio.css'
 
 const Portfolio = () => {
   return (
-    <div className='Portfolio__Container'>
-      <h3>Under Construction</h3>
-      <h2>Please Come Back Soon!</h2> 
+    <div>
+      <section className='Container__Portfolio'>
+
+        <div className='Headin__Portfolio'> 
+          <h2>My Projects</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid harum recusandae sequi quas. Ea debitis natus commodi asperiores iusto id?</p>
+        </div>
+
+      </section>
     </div>
   )
 }
