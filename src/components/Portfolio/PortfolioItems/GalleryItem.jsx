9ed03df3056f1c7c65f63cc9ad99d0c8c39fb0bf
Mansoor Hussain <mansoorhussain3053@gmail.com>
2024-01-18 01:55:23 -0800

    Small Change

--- src/components/Portfolio/PortfolioItems/GalleryItem.jsx
+++ src/components/Portfolio/PortfolioItems/GalleryItem.jsx
@@ -5,8 +5,10 @@ const GalleryItem = ({ item }) => {
     <div className="gallery-item">
       <img src={item.imageUrl} alt={item.title} />
       <div className="item-details">
+        <h4>{item.subtitle}</h4>
         <h3>{item.title}</h3>
         <p>{item.description}</p>
+        <button>{item.buttonText}</button>
       </div>
     </div>
   );
