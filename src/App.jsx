06a1b81d9319a34e04a9004eee3722f1975ed1b3
Mansoor Hussain <mansoorhussain3053@gmail.com>
2024-01-19 08:04:19 -0800

    Hambuger is in Process

--- src/App.jsx
+++ src/App.jsx
@@ -8,87 +8,83 @@ import Contact from "./components/Contact/Contact";
 import Services from "./components/Services/Services";
 import Portfolio from "./components/Portfolio/Portfolio";
 import Blog from "./components/Blog/Blog";
-import About from "./components/About/About"
+import About from "./components/About/About";
 
-
-
-function App() { 
+function App() {
   const portfolioItems = [
     {
-      title: 'Project 1',
-      description: 'Description for Project 1',
-      imageUrl: 'https://avatars.mds.yandex.net/i?id=dece3f518173d177ae4f17bf6d2b3891-4600186-images-thumbs&n=13',
-      category: 'category1',
+      title: "Project 1",
+      description: "Description for Project 1",
+      imageUrl:
+        "https://avatars.mds.yandex.net/i?id=dece3f518173d177ae4f17bf6d2b3891-4600186-images-thumbs&n=13",
+      category: "category1",
     },
     {
-      title: 'Project 2',
-      description: 'Description for Project 2',
-      imageUrl: 'https://avatars.mds.yandex.net/i?id=dece3f518173d177ae4f17bf6d2b3891-4600186-images-thumbs&n=13',
-      category: 'category2',
+      title: "Project 2",
+      description: "Description for Project 2",
+      imageUrl:
+        "https://avatars.mds.yandex.net/i?id=dece3f518173d177ae4f17bf6d2b3891-4600186-images-thumbs&n=13",
+      category: "category2",
     },
     {
-      title: 'Project 4',
-      description: 'Description for Project 2',
-      imageUrl: 'https://avatars.mds.yandex.net/i?id=dece3f518173d177ae4f17bf6d2b3891-4600186-images-thumbs&n=13',
-      category: 'category1',
-    }
+      title: "Project 4",
+      description: "Description for Project 2",
+      imageUrl:
+        "https://avatars.mds.yandex.net/i?id=dece3f518173d177ae4f17bf6d2b3891-4600186-images-thumbs&n=13",
+      category: "category1",
+    },
   ];
 
   return (
-
-    
     <>
-    
       <Header />
-      
-      <AnimatedCursor      
-        innerSize={24}
-        outerSize={20}
-        z-index={99}
-        color="69,242,252"
-        outerAlpha={0.2}
-        innerScale={0.7}
-        innerStyle={{
-          background:
-            "radial-gradient(circle, rgba(63,94,251,1) 0%, rgba(252,70,107,1) 100%)",
-        }}
-        outerScale={5}
-        clickables={[
-          "a",
-          'input[type="text"]',
-          'input[type="email"]',
-          'input[type="number"]',
-          'input[type="submit"]',
-          'input[type="image"]',
-          "label[for]",
-          "select",
-          "textarea",
-          "button",
-          ".link",
-          {
-            target: ".custom",
-            options: {
-              innerSize: 15,
-              outerSize: 15,
-              color: "131,58,180",
-              outerAlpha: 0.3,
-              innerScale: 0.7,
-              outerScale: 5,
+      <div className="cursor">
+        <AnimatedCursor
+          innerSize={24}
+          outerSize={20}
+          z-index={99}
+          color="69,242,252"
+          outerAlpha={0.2}
+          innerScale={0.7}
+          innerStyle={{
+            background:
+              "radial-gradient(circle, rgba(63,94,251,1) 0%, rgba(252,70,107,1) 100%)",
+          }}
+          outerScale={5}
+          clickables={[
+            "a",
+            'input[type="text"]',
+            'input[type="email"]',
+            'input[type="number"]',
+            'input[type="submit"]',
+            'input[type="image"]',
+            "label[for]",
+            "select",
+            "textarea",
+            "button",
+            ".link",
+            {
+              target: ".custom",
+              options: {
+                innerSize: 15,
+                outerSize: 15,
+                color: "131,58,180",
+                outerAlpha: 0.3,
+                innerScale: 0.7,
+                outerScale: 5,
+              },
             },
-          },
-        ]}
-      />
+          ]}
+        />
+      </div>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/contact" element={<Contact/>} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/services" element={<Services />} />
         <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/blog" element={<Blog/>} />
+        <Route path="/blog" element={<Blog />} />
         <Route path="/about" element={<About />} />
-
-        
       </Routes>
-      
       <Footer />
     </>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,106 +1,116 @@
-import  { useEffect, useState } from 'react';
-import './header.css'; 
+import React, { useEffect, useState } from "react";
+import "./header.css";
 import Logo from "../../assets/techlogo.webp";
 import { FaFacebook } from "react-icons/fa";
-import { BsTwitterX } from "react-icons/bs";
+import { IoLogoTwitter } from "react-icons/io5";
 import { FaLinkedin } from "react-icons/fa6";
 import { TbBrandFiverr } from "react-icons/tb";
 import { LuAlignCenter } from "react-icons/lu";
+
 import { Link } from "react-router-dom";
 
 const Header = () => {
   const [scrolling, setScrolling] = useState(false);
+  const [mobileMenuVisible, setMobileMenuVisible] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 0) {
-        setScrolling(true);
-      } else {
-        setScrolling(false);
-      }
+      setScrolling(window.scrollY > 0);
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
+  const toggleMobileMenu = () => {
+    setMobileMenuVisible(!mobileMenuVisible);
+  };
+
   return (
-    <header className={scrolling ? 'scrolling' : ''}>
-      <nav>
-       
-    <div className="Container__Header">
-      <div className="Header__Main">
-        <div className="Header__Logo">
-          <img src={Logo} alt="" />
-        </div>
-        <div className="Parent__Nav">
-          <div className="Header__Nav">
-            <ul className="Nav__Lists">
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/services">Services</Link>
-              </li>
-              <li>
-                <Link to="/portfolio">Portfolio</Link>
-              </li>
-              <li>
-                <Link to="/blog">Blog</Link>
-              </li>
-              <li>
-                <Link to="/about">About us</Link>
-              </li>
-              <li>
-                <Link to="/contact">Contact us</Link>
-              </li>
-            </ul>
-          </div>
+    <>
+      <header className={scrolling ? "scrolling" : ""}>
+        <nav>
+          <div className="Container__Header">
+            <div className="Header__Main">
+              <div className="Header__Logo">
+                <img src={Logo} alt="" />
+              </div>
 
-          <div className="Header__Button">
-          <Link to="/contact"><button className="btn Main">Get Started</button></Link>  
-          </div>
+              <div className="Parent__Nav">
+                <div className="Header__Nav">
+                  <ul
+                    className={`Nav__Lists ${
+                      mobileMenuVisible ? "mobile-menu-visible" : ""
+                    }`}
+                  >
+                    <li>
+                      <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                      <Link to="/services">Services</Link>
+                    </li>
+                    <li>
+                      <Link to="/portfolio">Portfolio</Link>
+                    </li>
+                    <li>
+                      <Link to="/blog">Blog</Link>
+                    </li>
+                    <li>
+                      <Link to="/about">About us</Link>
+                    </li>
+                    <li>
+                      <Link to="/contact">Contact us</Link>
+                    </li>
+                  </ul>
+                </div>
 
-          <div className="Header__Socials">
-            <ul className="Social__Lists">
-              <li>
-                <a href="#">
-                  <FaFacebook />
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <BsTwitterX />
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <TbBrandFiverr />
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <FaLinkedin />
-                </a>
-              </li>
-            </ul>
-          </div>
-          
-        </div>
-        <div className="Header__Toggle">
-            <LuAlignCenter className="Toggle__Button"/>
+                <div className="Header__Button">
+                  <Link to="/contact">
+                    <button className="btn Main">Get Started</button>
+                  </Link>
+                </div>
 
+                <div className="Header__Socials">
+                  <ul className="Social__Lists">
+                    <li>
+                      <a href="#">
+                        <FaFacebook />
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <IoLogoTwitter />
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <TbBrandFiverr />
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <FaLinkedin />
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+
+              <div className="Header__Toggle" onClick={toggleMobileMenu}>
+                <LuAlignCenter className="Toggle__Button" />
+              </div>
+            </div>
           </div>
-      </div>
-    </div>
-      </nav>
-    </header>
+        </nav>
+        <div
+          className={`humBarger ${mobileMenuVisible ? "show" : "hide"}`}
+        >    </div>
+      </header>
+    </>
   );
 };
 
 export default Header;
-
-
