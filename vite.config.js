77181ab5eac65c8818f28c97ed7adde32e01ef7a
Mansoor Hussain <mansoorhussain3053@gmail.com>
2024-01-21 18:29:50 +0500

    Revert

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// vite.config.js
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/techsolllc.net/',
-  publicDir: 'public',
   plugins: [react()],
-});
+})
