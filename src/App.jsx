6e80429d1c476a4f96b63d448eddb360dd0cd760
Mansoor Hussain <mansoorhussain3053@gmail.com>
2024-01-22 23:39:56 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import Services from "./components/Services/Services";
 import Portfolio from "./components/Portfolio/Portfolio";
 import Blog from "./components/Blog/Blog";
 import About from "./components/About/About";
-import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 
 
 
